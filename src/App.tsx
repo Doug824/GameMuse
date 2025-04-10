5b5f8019d370e25dcf40efeb14ad66a903060768
Doug824 <doug.hagan824@gmail.com>
2025-04-10 13:51:51 -0700

    inmorted header

--- src/App.tsx
+++ src/App.tsx
@@ -3,16 +3,20 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { FavoritesProvider } from './context/FavoritesContext';
 import Home from './Pages/Home';
 import GameDetails from './Pages/GameDetails';
+import Header from './components/Header';
 
 const App: React.FC = () => {
   return (
     <FavoritesProvider>
       <Router>
         <div className="min-h-screen bg-gray-900 text-white">
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/game/:id" element={<GameDetails />} />
-          </Routes>
+          <div className="container mx-auto px-4 py-6">
+            <Header />
+            <Routes>
+              <Route path="/" element={<Home />} />
+              <Route path="/game/:id" element={<GameDetails />} />
+            </Routes>
+          </div>
         </div>
       </Router>
     </FavoritesProvider>
