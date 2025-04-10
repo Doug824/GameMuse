c91482af7e8896b68dd3b89632566680828b7bd5
Doug824 <doug.hagan824@gmail.com>
2025-04-10 13:55:13 -0700

    home button

--- src/Pages/GameDetails.tsx
+++ src/Pages/GameDetails.tsx
@@ -158,14 +158,14 @@ const GameDetails: React.FC = () => {
         <div className="container mx-auto px-4 py-8">
             <div className="flex items-center justify-between mb-6">
                 <button 
-                    onClick={() => navigate(-1)}
-                    className="flex items-center text-gray-400 hover:text-white transition"
-                >
-                    <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5 mr-1" viewBox="0 0 20 20" fill="currentColor">
+                onClick={() => navigate(-1)}
+                className="flex items-center text-gray-400 hover:text-white mb-6 transition"
+            >
+                <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5 mr-1" viewBox="0 0 20 20" fill="currentColor">
                     <path fillRule="evenodd" d="M9.707 16.707a1 1 0 01-1.414 0l-6-6a1 1 0 010-1.414l6-6a1 1 0 011.414 1.414L5.414 9H17a1 1 0 110 2H5.414l4.293 4.293a1 1 0 010 1.414z" clipRule="evenodd" />
-                    </svg>
-                    Back
-                </button>
+                </svg>
+                Back
+            </button>
                 
                 <Link to="/" className="flex items-center text-xl font-bold transition hover:opacity-80">
                     <span className="text-purple-500">Game</span>
