8bea3256dfe6e7fd9854da891ad971d9935724af
Doug824 <doug.hagan824@gmail.com>
2025-04-10 15:42:17 -0700

    imported new background

--- src/App.tsx
+++ src/App.tsx
@@ -4,12 +4,13 @@ import { FavoritesProvider } from './context/FavoritesContext';
 import Home from './Pages/Home';
 import GameDetails from './Pages/GameDetails';
 import Header from './components/Header';
+import Background from './components/Background';
 
 const App: React.FC = () => {
   return (
     <FavoritesProvider>
       <Router>
-        <div className="min-h-screen bg-gray-900 text-white">
+        <Background>
           <div className="container mx-auto px-4 py-6">
             <Header />
             <Routes>
@@ -17,7 +18,7 @@ const App: React.FC = () => {
               <Route path="/game/:id" element={<GameDetails />} />
             </Routes>
           </div>
-        </div>
+        </Background>
       </Router>
     </FavoritesProvider>
   );
